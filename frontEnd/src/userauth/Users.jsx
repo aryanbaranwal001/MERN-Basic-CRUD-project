5d519c910db3b197fd1c139eac9f65d0b6f19a32
aryanbaranwal001 <aryanbaranwal131214@gmail.com>
2025-03-21 16:14:45 +0530

    made table and mapping data to jsx

--- frontEnd/src/userauth/Users.jsx
+++ frontEnd/src/userauth/Users.jsx
@@ -1,11 +1,60 @@
-
+import { useState } from "react";
 
 function Users() {
+  const [users, setUsers] = useState([
+    {
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+      age: 20,
+    },
+    {
+      name: "John Doe",
+      email: "johdddddddddddddddddddndoe@gmail.com",
+      age: 20,
+    },    
+  ]);
+
   return (
     <>
-      <p>Users</p>
+      <div className="bg-blue-500 justify-center items-center flex h-screen">
+        <div className="bg-white p-4 rounded-xl ">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Age</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              
+              {
+                users.map((user) => {
+                  return(
+                    <tr>
+                      <td>{user.name}</td>
+                      <td>{user.email}</td>
+                      <td>{user.age}</td>
+                      <td>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                      </td>
+                    </tr>
+
+                  );
+                })
+              }
+
+
+
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 }
 
-export { Users };  
+export { Users };
