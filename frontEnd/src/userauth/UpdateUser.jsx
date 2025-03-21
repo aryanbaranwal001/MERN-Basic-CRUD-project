a4d6b8ac26af25b440a92b59f0bb841f2fdd48c0
aryanbaranwal001 <aryanbaranwal131214@gmail.com>
2025-03-21 17:45:21 +0530

    frontEnd complete

--- frontEnd/src/userauth/UpdateUser.jsx
+++ frontEnd/src/userauth/UpdateUser.jsx
@@ -1,9 +1,48 @@
 
 
 function UpdateUser() {
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+  };
+
   return (
     <>
-      <p>UpdateUser</p>
+      <div className="bg-blue-500 justify-center items-center flex h-screen">
+        <div className="w-3/12 bg-white p-6 rounded-xl flex gap-2 flex-col">
+          
+          
+          <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+            <h2 className="text-3xl">Update User</h2>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              placeholder="Enter Name"
+              className="pl-3 py-1 block rounded-[4px] border w-full border-gray-200"
+            />
+
+            <label htmlFor="">Email</label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              className="pl-3 py-1 block rounded-[4px] border w-full border-gray-200"
+            />
+
+            <label htmlFor="">Age</label>
+            <input
+              type="text"
+              placeholder="Enter Age"
+              className="pl-3 py-1 block rounded-[4px] border w-full border-gray-200"
+            />
+            <div className="flex justify-center mt-2">
+              <button className="px-4 py-1 rounded-md text-white bg-green-600 min-w-3xl">
+                Update
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
--- frontEnd/src/userauth/Users.jsx
+++ frontEnd/src/userauth/Users.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
-
 function Users() {
   const [users, setUsers] = useState([
     {
@@ -20,7 +19,15 @@ function Users() {
     <>
       <div className="bg-blue-500 justify-center items-center flex h-screen">
         <div className="bg-white p-10 rounded-xl flex gap-2 flex-col">
-          <NavLink to="create" className={"rounded-md max-w-20 px-4 py-2 font-bold bg-green-600 text-white"} end>Add +</NavLink>
+          <NavLink
+            to="create"
+            className={
+              "rounded-md max-w-20 px-4 py-2 font-bold bg-green-600 text-white"
+            }
+            end
+          >
+            Add +
+          </NavLink>
           <table className="border border-gray-300 shadow-lg">
             <thead>
               <tr className="bg-gray-800 text-white">
@@ -29,7 +36,6 @@ function Users() {
                 <th className="px-4 py-2">Age</th>
                 <th className="px-4 py-2">Action</th>
               </tr>
-              
             </thead>
             <tbody>
               {users.map((user, index) => (
@@ -41,9 +47,16 @@ function Users() {
                   <td className="px-4 py-2 text-center">{user.email}</td>
                   <td className="px-4 py-2 text-center">{user.age}</td>
                   <td className="px-4 py-2 flex justify-center gap-2">
-                    <button className="px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600 transition">
+                    <NavLink
+                      to="update"
+                      className={
+                        "px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
+                      }
+                      end
+                    >
                       Edit
-                    </button>
+                    </NavLink>
+
                     <button className="px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600 transition">
                       Delete
                     </button>
